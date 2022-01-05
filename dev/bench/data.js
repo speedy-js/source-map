4c0f1e40e0cd11cc1aad5f9239832ac74fe05298
github-action-benchmark <github@users.noreply.github.com>
2022-01-05 15:00:06 +0000

    add Benchmark (benchmarkjs) benchmark result for cc91a5458d3e3c7f91e20ba8adf103f9b560285b

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1641394806750,
+  "repoUrl": "https://github.com/speedy-js/source-map",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "speedy-js",
+            "username": "speedy-js"
+          },
+          "committer": {
+            "name": "speedy-js",
+            "username": "speedy-js"
+          },
+          "id": "cc91a5458d3e3c7f91e20ba8adf103f9b560285b",
+          "message": "feat(core): basic impl",
+          "timestamp": "2022-01-04T09:32:35Z",
+          "url": "https://github.com/speedy-js/source-map/pull/1/commits/cc91a5458d3e3c7f91e20ba8adf103f9b560285b"
+        },
+        "date": 1641394803506,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "antd#@speedy-js/remapping",
+            "value": 6.01,
+            "range": "±0.94%",
+            "unit": "ops/sec",
+            "extra": "33 samples"
+          },
+          {
+            "name": "antd#@speedy-js/source-map",
+            "value": 8.49,
+            "range": "±0.43%",
+            "unit": "ops/sec",
+            "extra": "25 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
