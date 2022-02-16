366082bfdb57b35fcd3e4cbc117459396a1d68fb
h-a-n-a <andywangsy@gmail.com>
2022-02-16 16:25:19 +0800

    chore: add perf test

---
+++ benchmark/perf.cjs
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const path = require('path')
+const { SourceMap } = require('../node')
+
+const transformedMap = fs.readFileSync(
+  path.resolve(__dirname, './fixtures/antd/antd.js.map'),
+  'utf-8',
+)
+const minifiedMap = fs.readFileSync(
+  path.resolve(__dirname, './fixtures/antd/antd.min.js.map'),
+  'utf-8',
+)
+
+for (let i = 0; i < 3; i++) {
+  const mergeMaps = SourceMap.mergeMaps([minifiedMap, transformedMap])
+  mergeMaps.toMap()
+}
